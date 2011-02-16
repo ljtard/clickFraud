97f4b3af917694f719050945bcc23c967ad516d0
ljtard <livejournal.tard@gmail.com>
2011-02-16 21:40:25 +0600

    second

--- src/main.cpp
+++ src/main.cpp
@@ -6,20 +6,7 @@
 
 void myMessageOutput(QtMsgType type, const char *msg)
 {
-    switch (type) {
-    case QtDebugMsg:
-        printf("%s\n", msg);
-        break;
-    case QtWarningMsg:
-        printf("Warning: %s %s\n", QTime::currentTime().toString(Qt::TextDate).toAscii().data() , msg);
-        break;
-    case QtCriticalMsg:
-        printf("Critical: %s %s\n", QTime::currentTime().toString(Qt::TextDate).toAscii().data() , msg);
-        break;
-    case QtFatalMsg:
-        printf("Fatal: %s %s\n",QTime::currentTime().toString(Qt::TextDate).toAscii().data() , msg);
-        break;
-    }
+    printf("%s\n", msg);
 }
 
 
